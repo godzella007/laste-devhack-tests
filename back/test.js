807b6be24614c9f96e06620f13b174a8b99f7088
godzella007 <126893956+godzella007@users.noreply.github.com>
2023-06-07 01:00:26 +0200

    Update test.js

--- back/test.js
+++ back/test.js
@@ -1,31 +1,27 @@
-import React, { useEffect } from 'react';
-import assert from 'assert';
+const assert = require('assert');
 
-const ExampleComponent = () => {
-  useEffect(() => {
-    const runTests = async () => {
+const runTests = async () => {
+  try {
+    assert.strictEqual(2 + 2, 4);
+    await performAsyncOperation();
+    console.log('All tests passed!');
+  } catch (error) {
+    console.error('Test failed:', error);
+  }
+};
+
+const performAsyncOperation = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
       try {
-        assert.strictEqual(2 + 2, 4);
-        await performAsyncOperation();
-        console.log('All tests passed!');
+        assert.strictEqual(3 * 3, 9);
+        resolve();
       } catch (error) {
         console.error('Test failed:', error);
+        resolve();
       }
-    };
+    }, 1000);
+  });
+};
 
-    runTests();
-  }, []);
-
-  const performAsyncOperation = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        try {
-          assert.strictEqual(3 * 3, 9);
-          resolve();
-        } catch (error) {
-          console.error('Test failed:', error);
-          resolve();
-        }
-      }, 1000);
-    });
- 
+runTests();
